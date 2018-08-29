c2c1a155adc5239ba84e7e464f5f32691676dad1
Ryan Ewanicke <reiz.talsean@gmail.com>
2018-08-29 12:03:27 -0600

    refactored counting-characters.js to use a ternary operator

--- counting-characters.js
+++ counting-characters.js
@@ -3,17 +3,10 @@ function countLetters(input)
   tmpObj = {};
 
   for(let i = 0; i < input.length; i++)
-  {
-      if(tmpObj[input[i]] === undefined)
-      {
-        tmpObj[input[i]] = 1;
-      }
-      else
-      {
-        tmpObj[input[i]]++;
-      }
+  { 
+      tmpObj[input[i]] === undefined ? tmpObj[input[i]] = 1 :  tmpObj[input[i]]++;
   }
   return tmpObj;
 }
 
-console.log(countLetters("test"));
+console.log(countLetters("supercalafragilisticespialidosious123554"));
